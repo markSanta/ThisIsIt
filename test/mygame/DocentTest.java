aaa4c0b91b766e2fb0795330dc8f4e8018f3ffa2
jayson <dionas1010@gmail.com>
2014-12-08 02:16:00 +0100

    additing testing for docent and student

--- test/mygame/DocentTest.java
+++ test/mygame/DocentTest.java
@@ -28,6 +28,7 @@ public class DocentTest {
 
     @org.junit.Test
     public void testSomeMethod() {
+        System.out.println("Testing Docent")
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
